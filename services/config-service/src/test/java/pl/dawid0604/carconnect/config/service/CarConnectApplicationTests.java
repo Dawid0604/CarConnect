b9bd12ada66654ed003557b5b26b611ecf126277
Dawid Sikora <tadek123tadek@o2.pl>
2025-05-29 18:31:38 +0200

    🐛 fix: 100% tests cover

--- services/config-service/src/test/java/pl/dawid0604/carconnect/config/service/CarConnectApplicationTests.java
+++ services/config-service/src/test/java/pl/dawid0604/carconnect/config/service/CarConnectApplicationTests.java
@@ -8,9 +8,7 @@ class CarConnectApplicationTests {
 
 	@Test
 	void contextLoads() {
-		// This test is used to check if the Spring application context loads successfully.
-		// If there are any issues with the configuration or beans, this test will fail.
-		// No additional code is needed here as we are just verifying the context loading.
+		CarConnectApplication.main(new String[] { });
 	}
 
 }
